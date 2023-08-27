ea4d0107160a4072cafaeaf537b82c4bea7d4b17
Usman Qidwai <usmanqidwai21@gmail.com>
2023-08-27 17:21:21 -0400

    Looping completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function countDown(){
+    let number=0
+    while(number<=10){
+        console.log(number++)
+    }
+  
+}
+const names =["Guadalupe", "Ollie", "Aki"]
+function writeCards(names){
+    let name1 = []
+    for(let i=0; i<names.length;i++){
+        
+
+
+     name1.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+
+
+
+    
+    }
+   return name1
+
+}
+
+
+
